7c1efceb57a10c52f74757ea0fccb8ef368e6f8b
Devin Holland <devin.holland93@gmail.com>
2025-01-15 23:35:39 -0600

    react-router basename

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,6 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  // The base URL of the app
+  basename: "/henka/",
 } satisfies Config;
