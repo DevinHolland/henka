cae439f9fa8b8c425aa499b437fac9eb318b0606
Devin Holland <devin.holland93@gmail.com>
2025-01-16 18:57:48 -0600

    fix dockerfile for azure deploy

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,7 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: false,
-  // The base URL of the app
-  basename: "/henka/",
+  ssr: true,
 } satisfies Config;
