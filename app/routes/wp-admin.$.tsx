11fb3c42e1da9b8d5accb9e738e140c7c4b80cc7
Devin Holland <devin.holland93@gmail.com>
2025-01-23 21:59:26 -0600

    actually redirect bots

--- app/routes/wp-admin.$.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from "react-router";
-
-export async function serverLoader({}) {
-    redirect('https://media.tenor.com/uizXjlI9h0QAAAAM/youre-a-dick-rude.gif');
-}
-
-export default function WpAdmin() { return <></>}
